a2c918b0fddb85078ee8c57a06b4995a8dcc22bd
AmmarR0118 <88048568+AmmarR0118@users.noreply.github.com>
2022-01-06 15:26:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let weather = {
+    "apiKey": "01d12f3233cae9d959bd3ebeb174e211",
+    fetchWeather: function(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+        .catch((error) => this.displayError())
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search input").value)
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { description, icon } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".temp").innerText = `${parseInt(temp)}°C`;
+        document.querySelector(".image").src = `http://openweathermap.org/img/wn/${icon}.png`
+        document.querySelector(".description").innerText = `${description}`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".speed").innerText = `Wind speed: ${speed} km/h`;
+
+        document.querySelector(".weather").classList.remove("loading")
+        document.querySelector(".load").classList.remove("block")
+        document.querySelector(".error").style.display = "none";
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`
+    },
+    displayError: function() {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".load").classList.remove("block")
+    }
+}
+
+document.querySelector(".search button").addEventListener("click", function () {
+    document.querySelector(".load").classList.add("block")
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").classList.add("loading")
+    weather.search();
+})
+
+document.querySelector(".search").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        document.querySelector(".load").classList.add("block")
+        document.querySelector(".weather").classList.add("loading")
+        document.querySelector(".error").style.display = "none";
+        weather.search();
+    }
+})
